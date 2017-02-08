acbc7568fae18c91ada315d162f2d85343118c76
Jordan Shaw <jrdnshw@gmail.com>
2017-02-07 19:38:47 -0500

    updated to use class structure plus got all data publishing to pubnub

--- creation_by_error.ino
+++ creation_by_error.ino
@@ -1,6 +1,5 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
-//#include <ArduinoOTA.h>
 
 #include <Servo.h>
 #include <NewPingESP8266.h>
@@ -14,8 +13,16 @@
 #define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPingESP8266 sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-
 Servo myservo;  // create servo object to control a servo
+
+boolean isRunning = false;
+const int buttonPin = 2;
+int buttonState;             
+// the previous reading from the input pin
+int lastButtonState = HIGH;   
+
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 unsigned int pingSpeed = 33; // How frequently are we going to send out a ping (in milliseconds). 50ms would be 20 times a second.
 unsigned long pingTimer;     // Holds the next ping time.
@@ -32,93 +39,270 @@ int value = 0;
 
 SyncClient client;
 
+class Sweeper
+{
+  Servo servo;              // the servo
+  int id;
+  int pos;              // current servo position 
+  int increment;        // increment to move for each interval
+  int  updateInterval;      // interval between updates
+  unsigned long lastUpdate; // last update of position
+  String sweepString = "";
+  
+  NewPingESP8266 *sonar;
+  int currentDistance;
+
+  boolean publish_data = false;
+
+  // =============
+  // these two vars are pure debug variels to control what gets sent over serial
+  boolean sendJSON = true;
+  boolean storeDataJSON = true;
+  boolean printStringTitle = false;
+  // =============
+   
+public: 
+  Sweeper(int ide, int interval, NewPingESP8266 &sonar, int position)
+  {
+    updateInterval = interval;
+    // can be 1,2,3,4 etc...
+    increment = 2;
+    id = constrain(ide, 0, 13);
+    pos = position;
+  }
+  
+  void Attach(int pin)
+  {
+    if(servo.attached() == 0){
+      servo.attach(pin); 
+    }
+  }
+  
+  void Detach()
+  {
+    servo.detach();
+  }
+
+  void SetPos(int startPosition)
+  {
+    pos = startPosition;
+    servo.write(pos);
+  }
+
+  int isAttached()
+  {
+    return servo.attached();
+  }
+
+  void SetDistance(int d)
+  {
+    currentDistance = d;
+
+    if(storeDataJSON == true){
+      StoreData(currentDistance);
+    }
+  }
+
+  boolean GetPublishDataStatus()
+  {
+    return publish_data;
+  }
+
+  String GetPublishData()
+  {
+    return sweepString;
+  }
+
+  void ResetPublishDataStatus()
+  {
+    publish_data = false;
+    sweepString = "";
+  }
+
+  void SendBatchData() {
+    // helping debug the serial buffer issue
+    if(sendJSON == true){
+        if(sweepString.endsWith("/")){
+          int char_index = sweepString.lastIndexOf("/");
+          sweepString.remove(char_index);
+        }
+
+        if(printStringTitle == true){
+          Serial.println("");
+          Serial.print("sweepString: ");
+        }
+        Serial.println(sweepString);
+        publish_data = true;
+    }
+  }
+
+  void StoreData(int currentDistance)
+  {
+    if(printStringTitle == true){
+      if(String(currentDistance).length() > 0){
+        Serial.print("currentDistance: ");
+        Serial.print((String)currentDistance);
+        Serial.print(" ||||");
+        Serial.println(" ");
+      }
+    }
+
+    if(String(currentDistance).length() > 0){
+      String tmp = String(id);
+      tmp.concat(":");
+      tmp.concat(String(pos));
+      tmp.concat(":");
+      tmp.concat(String(currentDistance));
+      
+      sweepString.concat(tmp);
+      sweepString.concat("/");
+    }
+  }
+  
+  void Update()
+  {
+
+  // FINISH UPDATING THIS HERE!!!!
+//  if((millis() - movementTimer) > movementSpeed)  // time to update
+//  {
+//    movementTimer = millis();
+//    pos += increment;
+//    myservo.write(pos);
+//    if ((pos >= 170) || (pos <= 0))
+//    {
+//      SendBatchData();
+//      myservo.detach();
+//      myservo.attach(14);
+//      increment = -increment;
+//    }
+//  }
+
+    if (pos == -1) {
+      pos = 0;
+      servo.write(pos);
+    }
+
+    if((millis() - lastUpdate) > updateInterval)  // time to update
+    {
+      lastUpdate = millis();
+      pos += increment;
+      servo.write(pos);
+      if ((pos >= 170) || (pos <= 0)) // end of sweep
+      {
+        // send data through serial here
+        SendBatchData();
+        // reverse direction
+        Detach();
+        Attach(9);
+        increment = -increment;
+      }
+    }
+  }
+};
+
+Sweeper sweeper(0, 40, sonar, 0);
+
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 
   WiFi.begin(WIFI_AP, WIFI_PASSWORD);
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.printf("WiFi Failed!\n");
     return;
   }
+  
   Serial.printf("WiFi Connected!\n");
   Serial.println(WiFi.localIP());
-//  #ifdef ESP8266
-//    ArduinoOTA.begin();
-//  #endif
-  
-//  SyncClient client;
+
   const int l_httpPort = 80;
   if(!client.connect(g_host, l_httpPort)){
     Serial.println("Connect Failed");
     Serial.println("Pubnub Connection Failed");
     return;
   }
-//  client.setTimeout(2);
-//  if(client.printf("GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n") > 0){
-//    while(client.connected() && client.available() == 0){
-//      delay(1);
-//    }
-//    while(client.available()){
-//      Serial.write(client.read());
-//    }
-//    if(client.connected()){
-//      client.stop();
-//    }
-//  } else {
-//    client.stop();
-//    Serial.println("Send Failed");
-//    while(client.connected()) delay(0);
-//  }
   
   pingTimer = millis(); // Start now.
-  myservo.attach(14);  // attaches the servo on pin 9 to the servo object
+
+  sweeper.Attach(14);
+  sweeper.SetPos(90);
 }
 
+int measurement = 0;
+String JSONString = "";
+
 void loop() {
-  
-//  WiFiClient client;
-//  SyncClient client;
-//  const int l_httpPort = 80;
-//  if (!client.connect(g_host, l_httpPort)) {
-//    Serial.println("Pubnub Connection Failed");
-//    return;
-//  }
-  
-  // Notice how there's no delays in this sketch to allow you to do other processing in-line while doing distance pings.
-  if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
-    pingTimer += pingSpeed;      // Set the next ping time.
-    Serial.print("Ping: ");
-    Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-    Serial.println("cm");
+  int reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
   }
 
-  if((millis() - movementTimer) > movementSpeed)  // time to update
-  {
-    movementTimer = millis();
-    pos += increment;
-    myservo.write(pos);
-    if ((pos >= 170) || (pos <= 0))
-    {
-      myservo.detach();
-      myservo.attach(14);
-      increment = -increment;
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        isRunning = !isRunning;
+      }
     }
   }
 
-  if((millis() - requestTimer) > requestSpeed)
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
+
+  if(isRunning == true){
+    // update
+    if(sweeper.isAttached() == true){
+      sweeper.Update();
+    } else {
+      sweeper.Attach(9);
+      sweeper.Update();  
+    }
+  } else {
+    // check to make make sure the server is attached before detaching
+    if(sweeper.isAttached() == true){
+      sweeper.Detach();  
+    }
+  }
+
+  if(isRunning == true){
+    // Notice how there's no delays in this sketch to allow you to do other processing in-line while doing distance pings.
+    if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
+      pingTimer += pingSpeed;      // Set the next ping time.
+      Serial.print("Ping: ");
+      measurement = sonar.ping_cm();
+      sweeper.SetDistance(measurement);
+      Serial.print(measurement); // Send ping, get distance in cm and print result (0 = outside set distance range)
+      Serial.println("cm");
+    }
+  }
+
+  if(sweeper.GetPublishDataStatus() == true)
   {
-    requestTimer = millis();
     // Sending data to pubnub
     // ============
     //DATA FORMAT : http://pubsub.pubnub.com /publish/pub-key/sub-key/signature/channel/callback/message
   
-    String stringOne = "{\"num\":";
-    stringOne += value;
-    stringOne += ", \"txt\":\"Yo!\"}";
-    char charBuf[200];
-    stringOne.toCharArray(charBuf, 200);
+//    String stringOne = "{\"num\":";
+//    stringOne += value;
+//    stringOne += ", \"txt\":\"Yo!\"}";
+//    char charBuf[200];
+//    stringOne.toCharArray(charBuf, 200);
   
-    String json = "{\"text\":" + String(value) + "}";
+//    String json = "{\"text\":" + String(value) + "}";
+    String to_publish = sweeper.GetPublishData();
+//    String json = "{\"text\":" + String(to_publish) + "}";
+    String json = String(to_publish);
     
     String url = "/publish/";
     url += g_pubKey;
@@ -127,16 +311,12 @@ void loop() {
     url += "/0/";
     url += g_channel;
     url += "/0/";
-    url += urlencode(json);
+    url += "\"" + String(to_publish) + "\"";
+//    url += urlencode(json);
     
     Serial.println(url);
 
-//    if (!client.connect(g_host, l_httpPort)) {
-//      Serial.println("Connect Failed");
-//      return;
-//    }
     //client.setTimeout(2);
-//    if(client.printf("GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n") > 0){
     if(client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + g_host + "\r\n" + "Connection: keep-alive\r\n\r\n") > 0){
       Serial.println(client.available());
       Serial.println(client.connected());
@@ -157,26 +337,12 @@ void loop() {
       Serial.println("Send Failed");
       while(client.connected()) delay(0);
     }
-    
-//    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-//               "Host: " + g_host + "\r\n" + 
-//               "Connection: close\r\n\r\n");
-//    delay(10);
-//    
-//    while(client.available()){
-//      String line = client.readStringUntil('\r');
-//      Serial.print(line);
-//    }
 
     Serial.println();
-//    Serial.println("Pubnub Connection Closed");
+    sweeper.ResetPublishDataStatus();
   
     value++;
   }
-
-//  #ifdef ESP8266
-//    ArduinoOTA.handle();
-//  #endif
   
 }
 
